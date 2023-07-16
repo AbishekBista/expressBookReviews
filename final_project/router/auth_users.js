8f74f85f6b4203feae2fafcdcd4d586db39ab60a
AbishekBista <38556597+AbishekBista@users.noreply.github.com>
2023-07-16 14:11:34 +0545

    Update auth_users.js

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -10,19 +10,65 @@ const isValid = (username)=>{ //returns boolean
 }
 
 const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records.
+    const filteredUser = users.filter((user) => {
+        return user.username === username && user.password === password
+    })
+    return (filteredUser.length > 0)
 }
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const username = req.body.username
+  const password = req.body.password
+
+  if(!username || !password) {
+      return res.status(404).json({message: 'User credentials not provided'})
+  }
+
+  if(authenticatedUser(username, password)) {
+      let accessToken = jwt.sign({
+          data: password
+      }, 'access', {expiresIn: 60 * 60})
+
+      req.session.authorization = {
+          accessToken,
+          username
+      }
+
+      return res.status(200).json({message: 'User logged in successfully!'})
+  } else {
+      return res.status(208).json({message: 'Invalid login credentials!'})
+  }
 });
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn
+  
+
+  const bookReview = books[isbn]
+
+  if(bookReview) {
+    const {username} = req.session.authorization
+    const review = req.query.review
+
+
+    if(review) {
+
+        bookReview['review'] = {
+            ...bookReview['review'],
+            [username]: review
+        }
+    }
+
+    books[isbn] = bookReview
+
+    res.status(200).send(`Book with ISBN ${isbn} updated by ${username}`)
+  } else {
+      res.send(`Book with ISBN ${isbn} not found.`)
+  }
+
+  
 });
 
 module.exports.authenticated = regd_users;
